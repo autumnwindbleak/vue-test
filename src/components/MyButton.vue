2c3b0750eee03140c4de22677c4034be1d629f07
王雪洋 <autumnwindbleak@qq.com>
2021-05-26 14:29:32 +0800

    1st

---
+++ src/components/MyButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <Button>MyButton</Button>
+</template>
+
+<script>
+import { Button } from 'element-ui'
+
+export default {
+  name: "MyButton",
+  components: {
+    Button
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
