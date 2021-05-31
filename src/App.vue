a7533c29c95f7e1e7fe7e19b7b9dee496670386e
王雪洋 <autumnwindbleak@qq.com>
2021-05-31 11:56:27 +0800

    use center and zoom

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,58 @@
 <template>
-  <baidu-map
-      class='bm-view' ak='IK09ItUavVkbd6nMDUsfRLG3BsfHS2Rg'
-      :center='center'
-      :scroll-wheel-zoom="true"
-      @ready='handler'
-  >
-    <bm-geolocation
-        anchor="BMAP_ANCHOR_BOTTOM_RIGHT"
-        :showAddressBar="true"
-        :autoLocation="true"
-        @locationSuccess="logLocation"
-    />
-  </baidu-map>
+  <Container direction='vertical'>
+    <baidu-map
+        class='bm-view' ak='IK09ItUavVkbd6nMDUsfRLG3BsfHS2Rg'
+        :center='center'
+        :zoom='zoom'
+        :scroll-wheel-zoom="true"
+        @ready='handler'
+    >
+      <bm-geolocation
+          anchor='BMAP_ANCHOR_BOTTOM_RIGHT'
+          :showAddressBar='true'
+          :autoLocation='true'
+          @locationSuccess='logLocation'
+      />
+    </baidu-map>
+    <Button type='primary' @click="drawIcon">画图</Button>
+  </Container>
 </template>
 
 <script>
 import BaiduMap from 'vue-baidu-map/components/map/Map'
 import BmGeolocation from 'vue-baidu-map/components/controls/Geolocation'
+import { Container, Button } from 'element-ui'
+
+// 104.077967 30.549232
 export default {
   name: 'App',
   components: {
     BaiduMap,
-    BmGeolocation
+    BmGeolocation,
+    Button,
+    Container
   },
   data () {
     return {
-      center: {lng: 0, lat: 0},
+      //center是坐标的时候，zoom一定要设置，否则就不渲染，center是城市名字的时候zoom会被自动设置
+      center: {lng: 116.404, lat: 39.915},
+      zoom: 15
     }
   },
   methods: {
     handler ({BMap, map}) {
       console.log(BMap,map)
-      this.center = '北京'
+      //天府三街坐标
+      this.center = {lng: 104.075111, lat: 30.552450}
+      this.zoom = 15
     },
     logLocation({point, AddressComponent, marker}) {
       console.log(point)
       console.log(AddressComponent)
       console.log(marker)
+    },
+    drawIcon() {
+
     }
   },
 }
