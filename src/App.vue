9e6416f608e04ef6bd17586cb79ba1341565f245
王雪洋 <autumnwindbleak@qq.com>
2021-05-31 09:41:45 +0800

    baidu map init

--- src/App.vue
+++ src/App.vue
@@ -1,112 +1,40 @@
 <template>
-  <Container direction="vertical">
-    <Header>
-      this is header
-    </Header>
-    <Container>
-      <Aside width="200px">
-        this is aside
-      </Aside>
-      <Main>
-        <h1>{{this.message}}</h1>
-        <h2>Socket status: {{this.socketStatus}}</h2>
-        <Button type="primary" @click="link">Link</Button>
-        <Button type="secondary" @click="close">Disconnect</Button>
-      </Main>
-    </Container>
-  </Container>
+  <baidu-map
+      class='bm-view' ak='IK09ItUavVkbd6nMDUsfRLG3BsfHS2Rg'
+      :center='center'
+      :scroll-wheel-zoom="true"
+      @ready='handler'
+
+  />
 </template>
 
 <script>
-import { Aside, Container, Header, Main, Button} from "element-ui";
+import BaiduMap from 'vue-baidu-map/components/map/Map.vue'
+
 export default {
   name: 'App',
   components: {
-    Button,
-    Aside,
-    Header,
-    Main,
-    Container
+    BaiduMap
   },
-  data() {
+  data () {
     return {
-      path: 'ws://47.108.214.127:18092/test/one',
-      socket: '',
-      message: 'This is message',
-      socketStatus: 'closed'
+      center: {lng: 0, lat: 0},
+      // zoom: 3
     }
   },
   methods: {
-    link: function () {
-      if(typeof(WebSocket) === "undefined"){
-        alert("您的浏览器不支持socket")
-      }else{
-        // 实例化socket
-        this.socket = new WebSocket(this.path)
-        // 监听socket连接
-        this.socket.onopen = this.open
-        // 监听socket错误信息
-        this.socket.onerror = this.error
-        // 监听socket消息
-        this.socket.onmessage = this.getMessage
+    handler ({BMap, map}) {
+      console.log(BMap,map)
+      this.center = '成都'
 
-        this.socketStatus = 'linked'
-      }
-    },
-    open: function () {
-      console.log("socket连接成功")
-    },
-    error: function () {
-      console.log("连接错误")
-    },
-    getMessage: function (msg) {
-      this.message = msg.data
-      console.log(msg.data)
-    },
-    send: function () {
-      // this.socket.send(params)
-    },
-    close: function () {
-      this.socket.close()
-      this.socketStatus = 'closed'
-      console.log("socket已经关闭")
     }
   },
-  destroyed () {
-    // 销毁监听
-    this.socket.onclose = this.close
-  }
 }
 </script>
 
-<style>
-.el-header, .el-footer {
-  background-color: #0000000f;
-  color: #333;
-  text-align: center;
-  line-height: 60px;
-}
-.el-aside {
-  background-color: #ff00000f;
-  color: #333;
-  text-align: center;
-  line-height: 60px;
-}
-.el-main {
-  background-color: #00ff000f;
-  color: #333;
-  text-align: center;
-  line-height: 60px;
-}
-
-
-.reddiv {
-  background-color: red;
-}
-.bluediv {
-  background-color: blue;
-}
-.greendiv {
-  background-color: green;
-}
+<style scoped>
+  .bm-view {
+    width: 100%;
+    height: 500px;
+  }
 </style>
