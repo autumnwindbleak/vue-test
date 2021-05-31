6dd6a18dab23ef474509378ae1473bd6f77eeb58
王雪洋 <autumnwindbleak@qq.com>
2021-05-31 10:41:51 +0800

    fix a bug

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,39 @@
       :center='center'
       :scroll-wheel-zoom="true"
       @ready='handler'
-
-  />
+  >
+    <bm-geolocation
+        anchor="BMAP_ANCHOR_BOTTOM_RIGHT"
+        :showAddressBar="true"
+        :autoLocation="true"
+        @locationSuccess="logLocation"
+    />
+  </baidu-map>
 </template>
 
 <script>
-import BaiduMap from 'vue-baidu-map/components/map/Map.vue'
-
+import BaiduMap from 'vue-baidu-map/components/map/Map'
+import BmGeolocation from 'vue-baidu-map/components/controls/Geolocation'
 export default {
   name: 'App',
   components: {
-    BaiduMap
+    BaiduMap,
+    BmGeolocation
   },
   data () {
     return {
       center: {lng: 0, lat: 0},
-      // zoom: 3
     }
   },
   methods: {
     handler ({BMap, map}) {
       console.log(BMap,map)
-      this.center = '成都'
-
+      this.center = '北京'
+    },
+    logLocation({point, AddressComponent, marker}) {
+      console.log(point)
+      console.log(AddressComponent)
+      console.log(marker)
     }
   },
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import { Button } from "element-ui";
-
-// Vue.use(Button)
-// Vue.config.productionTip = false
+import 'element-ui/lib/theme-chalk/index.css'
 
 new Vue({
   render: h => h(App),
