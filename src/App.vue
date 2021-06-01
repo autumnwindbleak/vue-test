8a95b01d99f3c3b52d8668b8fcb6c45da2f3fcad
王雪洋 <autumnwindbleak@qq.com>
2021-06-01 11:54:04 +0800

    clear everything else and make main branch a clean branch as a base for everything

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <Container direction="vertical">
-    <Header>
+    <Header class="el-header">
       this is header
     </Header>
     <Container>
-      <Aside width="200px">
+      <Aside class="el-aside" width="200px">
         this is aside
       </Aside>
-      <Main>
-        <h1>{{this.message}}</h1>
-        <h2>Socket status: {{this.socketStatus}}</h2>
-        <Button type="primary" @click="link">Link</Button>
-        <Button type="secondary" @click="close">Disconnect</Button>
+      <Main class="el-main">
+        this is main
+        <Button type="primary">Primary Button</Button>
       </Main>
     </Container>
   </Container>
@@ -30,57 +28,17 @@ export default {
   },
   data() {
     return {
-      path: 'ws://47.108.214.127:18092/test/one',
-      socket: '',
-      message: 'This is message',
-      socketStatus: 'closed'
     }
   },
   methods: {
-    link: function () {
-      if(typeof(WebSocket) === "undefined"){
-        alert("您的浏览器不支持socket")
-      }else{
-        // 实例化socket
-        this.socket = new WebSocket(this.path)
-        // 监听socket连接
-        this.socket.onopen = this.open
-        // 监听socket错误信息
-        this.socket.onerror = this.error
-        // 监听socket消息
-        this.socket.onmessage = this.getMessage
-
-        this.socketStatus = 'linked'
-      }
-    },
-    open: function () {
-      console.log("socket连接成功")
-    },
-    error: function () {
-      console.log("连接错误")
-    },
-    getMessage: function (msg) {
-      this.message = msg.data
-      console.log(msg.data)
-    },
-    send: function () {
-      // this.socket.send(params)
-    },
-    close: function () {
-      this.socket.close()
-      this.socketStatus = 'closed'
-      console.log("socket已经关闭")
-    }
   },
   destroyed () {
-    // 销毁监听
-    this.socket.onclose = this.close
   }
 }
 </script>
 
 <style>
-.el-header, .el-footer {
+.el-header{
   background-color: #0000000f;
   color: #333;
   text-align: center;
@@ -99,14 +57,4 @@ export default {
   line-height: 60px;
 }
 
-
-.reddiv {
-  background-color: red;
-}
-.bluediv {
-  background-color: blue;
-}
-.greendiv {
-  background-color: green;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import { Button } from "element-ui";
+import 'element-ui/lib/theme-chalk/index.css';
 
-// Vue.use(Button)
-// Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
