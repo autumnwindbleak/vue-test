04ad46ec2e156ad55256ed642ac85324a05507d0
王雪洋 <autumnwindbleak@qq.com>
2021-06-01 16:11:22 +0800

    basic map useing leaflet

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,61 @@
 <template>
-  <Container direction="vertical">
-    <Header class="el-header">
+  <Container direction='vertical'>
+    <Header class='el-header'>
       this is header
     </Header>
     <Container>
-      <Aside class="el-aside" width="200px">
-        this is aside
+      <Aside class='el-aside' width='200px'>
+        Current Center is {{this.center}}
+        <Button type='primary' @click="onClick">Get Current Center</Button>
       </Aside>
-      <Main class="el-main">
-        this is main
-        <Button type="primary">Primary Button</Button>
+      <Main class='el-main'>
+        <div id='openstreetmap' ref='myMap'></div>
       </Main>
     </Container>
   </Container>
 </template>
 
 <script>
-import { Aside, Container, Header, Main, Button} from "element-ui";
+import { Aside, Container, Header, Main, Button } from 'element-ui';
+import L from 'leaflet'
+
 export default {
   name: 'App',
+  mounted() {
+    this.initMap()
+  },
   components: {
-    Button,
     Aside,
     Header,
     Main,
-    Container
+    Container,
+    Button
   },
   data() {
     return {
+      openStreetMap: null,
+      center: null
     }
   },
   methods: {
+    onClick() {
+      this.center = this.openStreetMap.getCenter()
+    },
+    initMap() {
+      this.openStreetMap = L.map('openstreetmap').setView([30.54610, 104.06854],15)
+      // add the OpenStreetMap tiles
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+      }).addTo(this.openStreetMap);
+    }
   },
   destroyed () {
   }
 }
 </script>
 
-<style>
+<style scoped>
 .el-header{
   background-color: #0000000f;
   color: #333;
@@ -56,5 +74,9 @@ export default {
   text-align: center;
   line-height: 60px;
 }
-
+#openstreetmap {
+  width: 100%;
+  height: 600px ;
+  z-index: 1;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
+//element ui css
 import 'element-ui/lib/theme-chalk/index.css';
+//leaflet css
+import 'leaflet/dist/leaflet.css'
 
-
+import App from './App.vue'
 new Vue({
   render: h => h(App),
 }).$mount('#app')
